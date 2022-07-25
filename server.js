7c7926907137b6b388614b7aa728cae4b76658b2
pjdtorres <paulo.torres@ipcb.pt>
2022-07-25 18:38:18 +0100

    esp32js250720221832

--- server.js
+++ server.js
@@ -1,21 +1,31 @@
 // main file
 
-const express = require("express");
+const express = require('express')
 const bodyParser = require('body-parser')
 require('dotenv').config()
-const app = expr
+const app = express()
+app.use(bodyParser.json())
 
 app.use(bodyParser.json()) // note: this is before the route
 
 const api = require('./rotas/')
 app.use('/api', api)
+
+// app.get('/', (req, res)=>{
+//     res.json({
+//         success: true
+//     })
+// })
+
+if(process.env.NODE_ENV === 'production'){ 
+    app.use(express.static('frontend/build'))
+
+    const path = require('path')
+    app.get('*', (req, res)=> {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    })
+}
+
 const PORT = process.env.PORT;
 // console.log(process.env.MONGO_PASS)
-
-app.get('/', (req, res)=>{
-    res.json({
-        success: true
-    })
-})
-
 app.listen(PORT)
